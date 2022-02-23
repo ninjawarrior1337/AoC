9886b71c2c16a2008b5b34fc72648fc890f2f21e
ninjawarrior1337 <t.rothled@gmail.com>
2022-02-23 14:26:02 -0800

    make runner generic

--- 2021/part2/runner.py
+++ 2021/part2/runner.py
@@ -1,12 +1,21 @@
+import sys
 import days
 from timeit import timeit
 from utils import AoCDay
 
-with open("input/d10.txt") as f:
-    day: AoCDay = getattr(days, "Day10")(f.read())
+def run(num: str):
+    with open(f"input/d{num}.txt") as f:
+        day: AoCDay = getattr(days, f"Day{num}")(f.read())
 
-    p1Time = timeit(day.part1, number=1)
-    print(f"Part 1: {day.p1}; Time: {p1Time}")
+        day.part1()
+        print(f"Part 1: {day.p1}")
 
-    day.part2()
-    print(f"Part 2: {day.p2}")
+        day.part2()
+        print(f"Part 2: {day.p2}")
+
+try:
+    dayNumber = sys.argv[1]
+    run(dayNumber)
+except:
+    print("Please write the number day you wish to run")
+    
