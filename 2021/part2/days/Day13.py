249c743a66203888f5aca87c7906c6cdd565ba9a
ninjawarrior1337 <t.rothled@gmail.com>
2022-02-26 23:14:20 -0800

    complete d13

--- 2021/part2/days/Day13.py
+++ 2021/part2/days/Day13.py
@@ -1,34 +1,51 @@
-from collections import defaultdict
 from utils import AoCDay
 import numpy as np
-
-from collections import defaultdict
- 
-# Utility function to create dictionary
-def multi_dict(K, type):
-    if K == 1:
-        return defaultdict(type)
-    else:
-        return defaultdict(lambda: multi_dict(K-1, type))
+from PIL import Image
 
 class Day13(AoCDay):
     grid: np.ndarray
-    points: list[tuple[int, int]]
-    folds = list[tuple[str, int]]
+    points: list[tuple[int, int]] = []
+    folds: list[tuple[str, int]] = []
 
     def __init__(self, linesRaw: str) -> None:
         super().__init__(linesRaw)
-        self.grid = multi_dict(2, int)
-        print(self.lines)
-        self.points = [(p[0], p[1]) for l in self.lines if self.lines.index(l) < self.lines.index('') for p in l.split(",")]
-        self.folds = [l[11:].split("=") for l in self.lines if self.lines.index(l) > self.lines.index('')]
-        xMax = max(self.points, key=lambda p: p[0])
-        yMax = max(self.points, key=lambda p: p[1])
-        grid = np.zeros((yMax, xMax))
+        for i, l in enumerate(self.lines):
+            if i < self.lines.index(""):
+                x, y = l.split(",")
+                self.points.append((int(x), int(y)))
+            elif i > self.lines.index(""):
+                direction, num = l[11:].split("=")
+                self.folds.append((direction, int(num)))
+        xMax = max(p[0] for p in self.points)
+        yMax = max(p[1] for p in self.points)
+        self.grid = np.zeros((yMax+1, xMax+1))
+        self.grid = np.hstack([self.grid, np.zeros((yMax+1, 3))])
         for p in self.points:
-            grid[p[1]][p[0]] = 1        
+            self.grid[p[1]][p[0]] = 1 
+
+    def fold(self, dir: str, num: int):
+        grid = self.grid[:]
+        
+        if dir == "x":
+            grid = np.transpose(grid)
+
+        upper = grid[0:num]
+        lower = grid[num+1:]
+        grid = upper + lower[::-1]
+        
+        if dir == "x":
+            grid = np.transpose(grid)
+        
+        self.grid = np.where(grid >= 1, 1, 0)
 
     def part1(self):
-        print(self.points)
-        print(self.folds)
+        print(self.grid.shape)
+        self.fold(*self.folds[0])
         print(self.grid)
+        self.p1 = np.sum(self.grid)
+
+    def part2(self):
+        for f in self.folds[1:]:
+            self.fold(*f)
+        im = Image.fromarray(np.uint8(self.grid)*255)
+        im.save("d13.png")
