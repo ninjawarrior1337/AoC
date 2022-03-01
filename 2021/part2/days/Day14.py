f17d90a66db0d345ef5a26fafb033c13358fd51f
ninjawarrior1337 <t.rothled@gmail.com>
2022-02-28 16:58:21 -0800

    almost there

--- 2021/part2/days/Day14.py
+++ 2021/part2/days/Day14.py
@@ -1,50 +1,47 @@
-from audioop import reverse
+from collections import defaultdict
 from typing import Counter
 from utils import AoCDay
 
 class Day14(AoCDay):
     base: list[str]
+    pairs: dict[tuple[str, str], int]
+    counts: dict[str, int]
     insertions: dict[str, str]
 
     def __init__(self, linesRaw: str) -> None:
         super().__init__(linesRaw)
         self.base = [c for c in self.lines[0]]
+        self.counts = defaultdict(Counter(self.base))
         self.insertions = {r.split(" -> ")[0]: r.split(" -> ")[1] for r in self.lines[2:] }
+        self.pairs = defaultdict(int)
+        self.compute_pairs()
 
-    def compute_insertions(self):
-        insertions = []
-        for i in self.insertions.items():
-            left, right = [c for c in i[0]]
-            for idx in range(len(self.base)-1, 0, -1):
-                if self.base[idx-1] == left and self.base[idx] == right:
-                    # self.base.insert(idx, "")
-                    insertions.append((idx, i[1]))
-        return insertions
+    def compute_pairs(self):
+        for i in range(1, len(self.base)):
+            left, right = self.base[i-1], self.base[i]
+            self.pairs[(left, right)] += 1
 
 
-    def apply_insertions(self, ins: list[tuple[int, str]]):
-        ins_sorted = sorted(ins, key=lambda i: i[0], reverse=True)
-        for i in ins_sorted:
-            self.base.insert(i[0], i[1])
+    def apply_insertions(self):
+        for p, v in list(self.pairs.items()):
+            for tmpl, val in self.insertions.items():
+                left, right = list(tmpl)
+                if p == (left, right):
+                    self.pairs[(left, val)] += 1 * v
+                    self.pairs[(val, right)] += 1 * v
+                    self.pairs[(left, right)] -= 1 * v
+                    self.counts[val] += 1
 
     def part1(self):
-        print(self.base)
+        print(self.pairs)
         print(self.insertions)
-        for _ in range(10):
-            ins = self.compute_insertions()
-            self.apply_insertions(ins)
-        c = Counter(self.base)
-        common = c.most_common()
-        self.p1 = common[0][1] - common[-1][1]
+        for i in range(5):
+            self.apply_insertions()
+        print(self.counts)
+        print(self.pairs)
+        print(sum(self.pairs.values()))
 
     def part2(self):
-        print(self.base)
-        print(self.insertions)
-        for _ in range(30):
-            ins = self.compute_insertions()
-            self.apply_insertions(ins)
-        c = Counter(self.base)
-        common = c.most_common()
-        self.p2 = common[0][1] - common[-1][1]
+        pass
         
         
