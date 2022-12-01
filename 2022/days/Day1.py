1808dc15f8799330f5d0be758332bda3b015e0a9
ninjawarrior1337 <me@treelar.xyz>
2022-11-30 21:41:45 -0800

    remove unnecessary collect

--- 2022/days/Day1.py
+++ 2022/days/Day1.py
@@ -2,6 +2,6 @@ from utils import AoCDay
 from itertools import groupby
 class Day1(AoCDay):
     def part1(self):
-        totals = [sum([int(v) for v in a]) for k, a in groupby(self.lines, lambda l: l != "") if k]
+        totals = [sum(int(v) for v in a) for k, a in groupby(self.lines, lambda l: l != "") if k]
         self.p1 = max(totals)
         self.p2 = sum(sorted(totals, reverse=True)[:3])
