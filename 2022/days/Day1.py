d46dfd861aaed1dadbb9bf6ca0f7189ad66bc9ed
ninjawarrior1337 <me@treelar.xyz>
2022-11-30 21:13:17 -0800

    complete d12022

--- 2022/days/Day1.py
+++ 2022/days/Day1.py
@@ -2,4 +2,21 @@ from utils import AoCDay
 
 class Day1(AoCDay):
     def part1(self):
-        self.p1 = 1000
+        totals = []
+        self.lines.append("")
+        t = 0
+        for v in self.lines:
+            if v != "":
+                i = int(v)
+                t += i
+            else:
+                totals.append(t)
+                t = 0
+        print(totals)
+        self.p1 = max(totals)
+
+        self.p2 = 0
+        for i in range(3):
+            self.p1 = max(totals)
+            self.p2 += self.p1
+            totals.remove(self.p1)
