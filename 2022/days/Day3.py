028bdb57bd9fa27c3b08822a245cc6a9932820bb
ninjawarrior1337 <me@treelar.xyz>
2022-12-02 21:42:06 -0800

    functional programming in python

--- 2022/days/Day3.py
+++ 2022/days/Day3.py
@@ -1,30 +1,29 @@
 from utils import AoCDay
-from itertools import groupby
-from more_itertools import grouper
+from more_itertools import grouper, divide
 from functools import reduce
+
 
 class Day3(AoCDay):
     def score(self, s: str):
-        if s.isupper():
-            return ord(s)-38
-        else:
-            return ord(s)-96
+        return ord(s)-38 if s.isupper() else ord(s)-96
 
     def part1(self):
-        total = 0
-        for l in self.lines:
-            a = set(l[:len(l)//2])
-            b = set(l[len(l)//2:])
-
-            print(a & b)
-            s = (a&b).pop()
-            total += self.score(s)
-
-        self.p1 = total
-
-        self.p2 = 0
-        for g in grouper(self.lines, 3):
-            sets = [set(v) for v in g]
-            s = reduce(lambda a, b: a&b, sets)
-            
-            self.p2 += sum(self.score(a) for a in s) 
+        self.p1 = sum(
+            map(
+                self.score,
+                (
+                    (set(divide(2, a)[0]) & set(divide(2, a)[1])).pop() for a in self.lines
+                )
+            )
+        )
+        self.p2 = sum(
+            map(
+                self.score,
+                [
+                    reduce(
+                        lambda a, b: a & b,
+                        [set(e) for e in g]
+                    ).pop() for g in grouper(self.lines, 3)
+                ]
+            )
+        )
