a378cab1aa7a72f679da3b06d9088206abc4b048
ninjawarrior1337 <me@treelar.xyz>
2022-12-03 21:43:29 -0800

    wow thats much cleaner

--- 2022/days/Day4.py
+++ 2022/days/Day4.py
@@ -3,29 +3,18 @@ from itertools import groupby
 
 class Day4(AoCDay):
     def part1(self):
-        res = []
+        self.p1 = 0
         self.p2 = 0
         for l in self.lines:
             o, t = l.split(",")
-            ar = range(int(o.split("-")[0]), int(o.split("-")[1])+1)
-            br = range(int(t.split("-")[0]), int(t.split("-")[1])+1)
-            
-            a = (int(o.split("-")[0]), int(o.split("-")[1]))
-            b = (int(t.split("-")[0]), int(t.split("-")[1]))
+            lo, ro = o.split("-")
+            lt, rt = t.split("-")
 
-            insidea = True
-            insideb = True
-            for v in ar:
-                if not b[0] <= v <= b[1]:
-                    insidea = False
-            for v in br:
-                if not a[0] <= v <= a[1]:
-                    insideb = False
-            if insidea or insideb:
-                print(l, insidea, insideb)
-            res.append(insidea or insideb)
+            As = set(range(int(lo), int(ro)+1))
+            Bs = set(range(int(lt), int(rt)+1))
 
-            if len(set(ar) & set(br)) > 0:
+            if len(As-Bs) == len(As)-len(Bs) or len(Bs-As) == len(Bs)-len(As):
+                self.p1 += 1
+
+            if len(As & Bs) > 0:
                 self.p2 += 1
-        
-        self.p1 = sum(1 for v in res if v)
