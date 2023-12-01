745a736fde572d007b202fcbf3ce3303627b8402
ninjawarrior1337 <me@treelar.xyz>
2023-12-01 01:15:50 -0800

    ended up using a solution from reddit to complete

--- 2023/src/d1.rs
+++ 2023/src/d1.rs
@@ -1,29 +1,68 @@
+use std::collections::HashMap;
+
 use crate::AoCDay;
 use aoc_macros::AoCSetup;
+use nom::{InputIter, FindSubstring};
 
 #[derive(AoCSetup, Default)]
-pub struct D1 {
-    packs: Vec<i32>,
+pub struct D1 {}
+
+fn extract_nums(s: String) -> (u32, u32) {
+    let v: Vec<char> = s.chars()
+        .filter(|c| c.is_numeric())
+        .collect();
+
+    return (str::parse(&v.first().unwrap().to_string()).unwrap(), str::parse(&v.last().unwrap().to_string()).unwrap())
+}
+
+fn extract_words(s: &str) -> (u32, u32) {
+    let num_map: HashMap<&str, &str> = [
+        ("one", "one1one"),
+        ("two", "two2two"),
+        ("three", "three3three"),
+        ("four", "four4four"),
+        ("five", "five5five"),
+        ("six", "six6six"),
+        ("seven", "seven7seven"),
+        ("eight", "eight8eight"),
+        ("nine", "nine9nine"),
+    ].iter().cloned().collect();
+    // let mut replace_order = num_map.keys()
+    //     .map(|&k| (s.find_substring(k), k))
+    //     .filter(|f| f.0.is_some())
+    //     .map(|f| (f.0.unwrap(), f.1))
+    //     .collect::<Vec<_>>();
+
+    // replace_order.sort_by_key(|e| e.0);
+
+    let mut new_s = s.to_owned();
+    for (&k, &v) in num_map.iter() {
+
+        new_s = new_s.replace(k, v)
+    }
+    extract_nums(new_s)
 }
 
 impl AoCDay for D1 {
     fn part1(&mut self) {
         let s: &str = self.input();
-        self.packs = s
-            .split("\n\n")
-            .map(|e| {
-                e.split("\n")
-                    .map(|c| c.parse::<i32>().unwrap())
-                    .sum::<i32>()
-            })
-            .collect();
+        
+        let a = s.split("\n")
+        .map(|s| extract_nums(s.to_owned()))
+        .map(|(d1, d2)| d1*10 + d2)
+        .sum::<u32>();
 
-        println!("{}", self.packs.iter().max().unwrap())
+        println!("{}", a)
     }
 
     fn part2(&mut self) {
-        self.packs.sort();
+        let s: &str = self.input();
+        
+        let a = s.split("\n")
+        .map(|s| extract_words(s))
+        .map(|(d1, d2)| d1*10 + d2)
+        .sum::<u32>();
 
-        println!("{}", self.packs.iter().rev().take(3).sum::<i32>())
+        println!("{}", a)
     }
 }
