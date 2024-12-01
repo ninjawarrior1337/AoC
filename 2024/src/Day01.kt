cc1843a575a9a704490a96978320f2e4cefeb611
Treelar <me@treelar.xyz>
2024-11-30 21:08:28 -0800

    2024d1: initial solution

--- 2024/src/Day01.kt
+++ 2024/src/Day01.kt
@@ -1,14 +1,25 @@
+import kotlin.math.absoluteValue
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var list1 = input.map { it.split("   ") }.map { it[0].toInt() }
+        var list2 = input.map { it.split("   ") }.map { it[1].toInt() }
+
+        list1 = list1.sorted()
+        list2 = list2.sorted()
+
+        return list1.zip(list2).map { (it.first - it.second).absoluteValue }.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val list1 = input.map { it.split("   ") }.map { it[0].toInt() }
+        val list2 = input.map { it.split("   ") }.map { it[1].toInt() }
+
+        return list1.sumOf { it * list2.count { a -> a == it } }
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+//    check(part1(listOf("test_input")) == 1)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
