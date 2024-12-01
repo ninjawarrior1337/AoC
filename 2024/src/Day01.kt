f65a78a2ed00da27ea825b64aa12b54fa9481f44
Treelar <me@treelar.xyz>
2024-11-30 22:32:31 -0800

    2024d1: a little nicer ig

--- 2024/src/Day01.kt
+++ 2024/src/Day01.kt
@@ -1,27 +1,23 @@
 import kotlin.math.absoluteValue
 
 fun main() {
+    fun extractLeftRight(input: List<String>): Pair<List<Int>, List<Int>> {
+        return Pair(
+            input.map { it.split("   ") }.map { it[0].toInt() },
+            input.map { it.split("   ") }.map { it[1].toInt() }
+        )
+    }
+
     fun part1(input: List<String>): Int {
-        var list1 = input.map { it.split("   ") }.map { it[0].toInt() }
-        var list2 = input.map { it.split("   ") }.map { it[1].toInt() }
-
-        list1 = list1.sorted()
-        list2 = list2.sorted()
-
-        return list1.zip(list2).map { (it.first - it.second).absoluteValue }.sum()
+        val (left, right) = extractLeftRight(input)
+        return left.sorted().zip(right.sorted()).sumOf { (it.first - it.second).absoluteValue }
     }
 
     fun part2(input: List<String>): Int {
-        val list1 = input.map { it.split("   ") }.map { it[0].toInt() }
-        val list2 = input.map { it.split("   ") }.map { it[1].toInt() }
-
-        return list1.sumOf { it * list2.count { a -> a == it } }
+        val (left, right) = extractLeftRight(input)
+        return left.sumOf { it * right.count { a -> a == it } }
     }
 
-    // Test if implementation meets criteria from the description, like:
-//    check(part1(listOf("test_input")) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
