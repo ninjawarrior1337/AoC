ee643ebe47cf62b6343ec08ef192c489fbbf25fb
ninjawarrior1337 <me@treelar.xyz>
2025-12-11 09:50:49 -0800

    effect comes to save the day and JSON.stringify costs me 3 hours

--- 2025/d11.ts
+++ 2025/d11.ts
@@ -1,4 +1,5 @@
-import { memoize, unlines } from "./utils";
+import { Cache, Data } from "effect";
+import { memoize, memoizeData, unlines } from "./utils";
 
 type Graph = {
   [key: string]: string[];
@@ -16,16 +17,23 @@ for (const l of lines) {
   g[node!] = edges;
 }
 
+type CacheKey = {
+  start: string,
+  end: string
+}
+
+const mkCacheKey = Data.case<CacheKey>()
+
 const countPaths = (graph: Graph) => {
-  const fn = memoize(
-    ({ start, _end: end }: { start: string; _end: string }): number => {
+  const fn = memoizeData(
+    ({ start, end }: CacheKey): number => {
       if (start === end) {
         return 1;
       }
 
       const edges = graph[start];
       return edges
-        ?.map((n) => fn({ start: n, _end: end }))
+        ?.map((n) => fn(mkCacheKey({ start: n, end })))
         .reduce((acc, v) => acc + v) ?? 0;
     }
   );
@@ -33,58 +41,14 @@ const countPaths = (graph: Graph) => {
   return fn;
 };
 
-// const countPathsContaining = (graph: Graph) => {
-//   const fn = memoize(
-//     ({
-//       start,
-//       _end,
-//       seen_dac,
-//       seen_fft,
-//     }: {
-//       start: string;
-//       _end: string
-//       seen_dac: boolean;
-//       seen_fft: boolean;
-//     }): number => {
-//       if (start === "out") {
-//         if (seen_dac && seen_fft) {
-//           return 1;
-//         }
-//         return 0;
-//       }
-
-//       const edges = graph[start]!;
-
-//       const ret = edges
-//         .map((n) => {
-//           return fn({
-//             start: n,
-//             _end,
-//             seen_dac: seen_dac || start === "dac",
-//             seen_fft: seen_fft || start === "fft",
-//           });
-//         })
-//         .reduce((acc, v) => acc + v);
-//       return ret;
-//     }
-//   );
-
-//   return fn;
-// };
-
 const countPathsG = countPaths(g);
-console.log(countPathsG({ start: "you", _end: "out" }));
+console.log(countPathsG({ start: "you", end: "out" }));
 
 console.log(
-  countPathsG({ start: "svr", _end: "dac" }) *
-    countPathsG({ start: "dac", _end: "fft" }) *
-    countPathsG({ start: "fft", _end: "out" }) +
-    countPathsG({ start: "svr", _end: "fft" }) *
-      countPathsG({ start: "fft", _end: "dac" }) *
-      countPathsG({ start: "dac", _end: "out" })
+  countPathsG({ start: "svr", end: "dac" }) *
+    countPathsG({ start: "dac", end: "fft" }) *
+    countPathsG({ start: "fft", end: "out" }) +
+    countPathsG({ start: "svr", end: "fft" }) *
+      countPathsG({ start: "fft", end: "dac" }) *
+      countPathsG({ start: "dac", end: "out" })
 );
-
-// const countPathsCt = countPathsContaining(g);
-// console.log(
-//   countPathsCt({ start: "svr", _end: "out", seen_dac: false, seen_fft: false })
-// );
--- 2025/utils.ts
+++ 2025/utils.ts
@@ -1,3 +1,5 @@
+import { MutableHashMap } from "effect";
+
 export const parseGridAs = <T>(input: string): T => {
     return input.split("\n").map(v => v.split("")) as T
 }
@@ -7,7 +9,7 @@ export const unlines = (input: string) => input.split("\n")
 export const memoize = <K extends { [k: string]: any }, V>(f: (arg: K) => V) => {
   const memo = new Map<string, V>();
   return (arg: K) => {
-    console.log(memo)
+    // console.log(memo)
     const fKey = Object.keys(arg).filter(v => !v.startsWith("_")).reduce((obj, key) => Object.assign(obj, {[key]: arg[key]}), {})
     const tKey = JSON.stringify(fKey, Object.keys(fKey).sort());
     const c = memo.get(tKey);
@@ -18,4 +20,19 @@ export const memoize = <K extends { [k: string]: any }, V>(f: (arg: K) => V) => 
     memo.set(tKey, res);
     return res;
   };
+}
+
+export const memoizeData = <K, V>(f: (arg: K) => V) => {
+  let memo = MutableHashMap.make()
+
+  return (arg: K): V => {
+    const c = MutableHashMap.get(memo, arg)
+    if(c._op === "Some") {
+      return c.value as V
+    } else {
+      const res = f(arg)
+      memo = MutableHashMap.set(memo, arg, res)
+      return res
+    }
+  }
 }
